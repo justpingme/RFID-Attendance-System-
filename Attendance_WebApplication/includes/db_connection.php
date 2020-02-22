b81e3fd3dc6aec0f2f7fc94db2160eb90084d906
Abhinay raj <31544581+abhinayraj1226@users.noreply.github.com>
2020-02-22 22:27:14 +0530

    Update db_connection.php

--- Attendance_WebApplication/includes/db_connection.php
+++ Attendance_WebApplication/includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-    define("DB_SERVER", "sql313.888webhost.com");
-    define("DB_USER", "888we_21419398");
-    define("DB_PASS", "abhinayraj9755");
-    define("DB_NAME", "888we_21419398_abhinayraj");
+    define("DB_SERVER", " ");
+    define("DB_USER", " ");
+    define("DB_PASS", " ");
+    define("DB_NAME", " ");
 
 // CREATE DATABASE CONNECTION
 
